d4a50f6557fbe90b200a6d749bebe775f72b1110
jvitords <jvduarte.santos@hotmail.com>
2024-12-11 23:46:40 -0300

    Adicionando todo código

--- ExercicioDeArquivos/src/model/entities/Produto.java
+++ ExercicioDeArquivos/src/model/entities/Produto.java
@@ -2,4 +2,44 @@ package model.entities;
 
 public class Produto {
 
+	String nomeDoProduto;
+	Double valorDoProduto;
+	Integer quantidadeDoProduto;
+	
+	public Produto() {
+	}
+
+	public Produto(String nomeDoProduto, Double valorDoProduto, Integer quantidadeDoProduto) {
+		this.nomeDoProduto = nomeDoProduto;
+		this.valorDoProduto = valorDoProduto;
+		this.quantidadeDoProduto = quantidadeDoProduto;
+	}
+
+	public String getNomeDoProduto() {
+		return nomeDoProduto;
+	}
+
+	public void setNomeDoProduto(String nomeDoProduto) {
+		this.nomeDoProduto = nomeDoProduto;
+	}
+
+	public Double getValorDoProduto() {
+		return valorDoProduto;
+	}
+
+	public void setValorDoProduto(Double valorDoProduto) {
+		this.valorDoProduto = valorDoProduto;
+	}
+
+	public Integer getQuantidadeDoProduto() {
+		return quantidadeDoProduto;
+	}
+
+	public void setQuantidadeDoProduto(Integer quantidadeDoProduto) {
+		this.quantidadeDoProduto = quantidadeDoProduto;
+	}
+	
+	public Double valorTotal() {
+		return valorDoProduto * quantidadeDoProduto;
+	}
 }
